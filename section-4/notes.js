6833a6cb347eee124ba577770190c310492eb820
Luke Fleming <lukefleming@lukes-mbp.myfiosgateway.com>
2022-06-15 14:36:05 -0400

    Section 4 Complete

---
+++ section-4/notes.js
@@ -0,0 +1,41 @@
+// Algorithim is a process or a set of tasks to accomplish a certain task
+
+/*
+
+How do you improve?
+
+1. Devise a plan for solving problems
+2. Master common problem solving problems
+
+*/
+
+/*
+
+How to Problem Solve?
+
+- Understand the Problem
+    1. Can I restate the problem in my own words?
+    2. What are the inputs that go into the problem?
+    3. What are the outputs that should come from the problem?
+    4. Can the outputs be determined from the inputs? In other words, do I have enough information to solve the problem?
+    5. How should I label the important pieces of data that are a part of the problem?
+- Explore Concrete Examples
+- Break It Down
+- Solve / Simplify
+- Look Back and Refactor
+    1. Can you check the result?
+    2. Can you derive the result differently?
+    3. Can you understand it at a glance?
+    4. Can you use the result or method for some other problem?
+    5. Can you improve the performance of your solution?
+    6. Can you think of other ways to refactor?
+    7. How have other people solved this problem?
+
+*/
+
+function addTwoNumbers(x,y) {
+    return x + y
+}
+
+console.log(addTwoNumbers(7,3))
+
