cc9b3bad402171fcd57a31135e7e2f06479f7592
Luke Fleming <lukefleming@Lukes-MacBook-Pro.local>
2022-06-14 20:47:56 -0400

    Second Commit

--- section-2/notes.js
+++ section-2/notes.js
@@ -7,3 +7,20 @@ const reverseString = (word) => {
 }
 
 console.log(reverseString('luke'))
+
+function addUpTo(n) {
+    let total = 0;
+    for(let i = 1; i <= n; i++) {
+        total += i
+    }
+    return total
+}
+
+function addUpToComplex(n) {
+    return n * (n + 1) / 2
+}
+
+let t1 = performance.now()
+addUpToComplex(1000000000)
+let t2 = performance.now()
+console.log(`Time Elapsed: ${(t2-t1) / 1000} seconds.`)
