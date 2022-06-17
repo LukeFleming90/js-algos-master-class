d8f6b32a2119ead0e7d5d1fe4bc8a30a476d2ce4
Luke Fleming <lukefleming@lukes-mbp.myfiosgateway.com>
2022-06-17 13:31:06 -0400

    Anagram Check Complete

--- section-5/notes.js
+++ section-5/notes.js
@@ -33,4 +33,39 @@ function same(arr1,arr2) {
 
 }
 
-console.log(same([1,2,3,2,3],[1,4,9,4,9]))
+// console.log(same([1,2,3,2,3],[1,4,9,4,9]))
+
+function anagramCheck(str1,str2) {
+
+    if(str1.length !== str2.length) {
+        return false;
+    }
+
+    let frequencyCounter1 = {}
+    let frequencyCounter2 = {}
+
+    for(let val of str1) {
+        frequencyCounter1[val] = (frequencyCounter1[val] || 0) + 1
+    }
+
+    for(let val of str2) {
+        frequencyCounter2[val] = (frequencyCounter2[val] || 0) + 1
+    }
+
+    for(let key in frequencyCounter1) {
+        if(frequencyCounter1[key] !== frequencyCounter2[key]) {
+            return false
+        }
+    }
+
+    return true
+
+}
+
+console.log(anagramCheck('aaz','zza'))
+console.log(anagramCheck('rat','car'))
+console.log(anagramCheck('qwerty','qeywrt'))
+console.log(anagramCheck('',''))
+console.log(anagramCheck('anagram','nagaram'))
+console.log(anagramCheck('awesome','awesom'))
+console.log(anagramCheck('texttwisttime','timetwisttext'))
