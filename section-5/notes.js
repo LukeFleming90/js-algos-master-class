57e2dee243994319167df26fd1290111e94c474c
Luke Fleming <LukeFleming90@gmail.com>
2022-06-24 14:58:45 -0400

    Section 5 completed

--- section-5/notes.js
+++ section-5/notes.js
@@ -85,4 +85,64 @@ function sumZero(arr) {
     }
 }
 
-console.log(sumZero([-4,-3,-2,-1,0,1,2,3,10]))
+function countUniqueValues(arr) {
+    if(arr.length === 0) return 0
+    var i = 0;
+    for(var j = 1; j < arr.length; j++) {
+        if(arr[i] !== arr[j]) {
+            i++;
+            arr[i] = arr[j]
+        }
+    }
+    return i + 1
+}
+
+
+console.log(countUniqueValues([1,1,1,2,2,3,4,5,5,5,6,7]))
+
+// Sliding Window
+
+function maxSubarraySum(arr,num) {
+    let maxSum = 0;
+    let tempSum = 0;
+    if(arr.length < num) return null;
+    for(let i = 0; i < num; i++) {
+        maxSum += arr[i];
+    }
+    tempSum = maxSum;
+    for(let i = num; i < arr.length; i++) {
+        tempSum = tempSum - arr[i - num] + arr[i]
+        maxSum = Math.max(maxSum, tempSum)
+    }
+    return maxSum
+}
+
+console.log(maxSubarraySum([2,6,9,2,1,8,5,6,3],3))
+
+// Binary Search
+
+function search(array, val) {
+
+    let min = 0;
+    let max = array.length - 1;
+
+    while (min <= max) {
+        let middle = Math.floor((min + max) / 2);
+        let currentEelement = array[middle];
+
+        if(array[middle] < val) {
+            min = middle + 1
+        }
+
+        else if(array[middle] > val) {
+            max = middle - 1
+        }
+
+        else {
+            return middle
+        }
+    }
+
+    return -1;
+
+}
